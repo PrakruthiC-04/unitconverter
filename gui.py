14ba34b2090f40a8492bf165569a601866360be6
Prakruthi C <prakruthic.ch25@bmsce.ac.in>
2025-12-17 22:50:32 +0530

    updated code

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 import os
 import time
 
-conversion_codes={
+basic_codes={
     "1-Centimetre to Metre":1,
     "2-Metre to Centimetre":2,
     "3-Metre to Kilometre":3,
@@ -23,6 +23,32 @@ conversion_codes={
     "16-Kelvin to Celsius":16,
     "17-Litre to Millilitre":17,
     "18-Millilitre to Litre":18
+}
+
+complex_codes={
+    "19-Joule to Calorie":19,
+    "20-Calorie to Joule":20,
+    "21-Pascal to Bar":21,
+    "22-Bar to Pascal":22,
+    "23-Pascal to psi":23,
+    "24-psi to pascal":24,
+    "25-Torr to Pascal":25,
+    "26-Pascal to Torr":26,
+    "27-m/s to kmph":27,
+    "28-kmph to m/s":28,
+    "29-Mile/s to kmph":29,
+    "30-kmph to Mile/s":30,
+    "31-g/cubic cm to Kg/cubic m":31,
+    "32-Kg/cubic m to g/cubic cm":32,
+    "33-Horsepower to Watt":33,
+    "34-Watt to Horsepower":34,
+    "35-Megabyte/s to Megabit/s":35,
+    "36-Megabit/s to Megabyte/s":36,
+    "37-Mg/dL to mmol/L":37,
+    "38-mmol/L to Mg/dL":38,
+    "39-gold purity(%)":39,
+    "40-knot to kmph":40,
+    "41-kmph to knot":41
 }
 
 def perform_conversion_via_files(value,conversion_code):
@@ -67,42 +93,67 @@ def perform_conversion_via_files(value,conversion_code):
 class ConverterApp:
     def __init__(self, root):
         self.root = root
-        root.title("Unit Converter (C Logic)")
+        root.title("Advanced Unit Converter")
+        root.geometry("600x550") 
         
-        main_frame = ttk.Frame(root, padding="10 10 10 10")
+        main_frame = ttk.Frame(root, padding="20")
         main_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-        
-        ttk.Label(main_frame, text="Value to Convert:").grid(column=1, row=1, sticky=tk.W)
-        self.input_entry = ttk.Entry(main_frame, width=20)
+    
+        ttk.Label(main_frame, text="Value to Convert:").grid(column=1, row=1, sticky=tk.W, pady=10)
+        self.input_entry = ttk.Entry(main_frame, width=25)
         self.input_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
         
-        ttk.Label(main_frame, text="Select Conversion:").grid(column=1, row=2, sticky=tk.W)
-        
-        self.dropdown_variable = tk.StringVar(root)
-        self.dropdown_variable.set(list(conversion_codes.keys())[0]) 
-        
-        self.conversion_menu = ttk.Combobox(main_frame, 
-                                            textvariable=self.dropdown_variable, 
-                                            values=list(conversion_codes.keys()),
-                                            state='readonly', 
-                                            width=30)
-        self.conversion_menu.grid(column=2, row=2, sticky=(tk.W, tk.E))
+        ttk.Label(main_frame, text="Basic Conversions:").grid(column=1, row=2, sticky=tk.W, pady=5)
+        self.basic_var = tk.StringVar()
+        self.basic_menu = ttk.Combobox(main_frame, 
+                                        textvariable=self.basic_var, 
+                                        values=list(basic_codes.keys()),
+                                        state='readonly', 
+                                        width=40)
+        self.basic_menu.grid(column=2, row=2, sticky=(tk.W, tk.E))
+        self.basic_menu.bind("<<ComboboxSelected>>", lambda e: self.complex_var.set(""))
+
+        ttk.Label(main_frame, text="Complex Conversions:").grid(column=1, row=3, sticky=tk.W, pady=5)
+        self.complex_var = tk.StringVar()
+        self.complex_menu = ttk.Combobox(main_frame, 
+                                          textvariable=self.complex_var, 
+                                          values=list(complex_codes.keys()),
+                                          state='readonly', 
+                                          width=40)
+        self.complex_menu.grid(column=2, row=3, sticky=(tk.W, tk.E))
+        self.complex_menu.bind("<<ComboboxSelected>>", lambda e: self.basic_var.set(""))
 
         self.convert_button = ttk.Button(main_frame, text="CONVERT", 
                                          command=self.run_conversion)
-        self.convert_button.grid(column=1, row=3, columnspan=2, pady=10) 
-        self.result_label = ttk.Label(main_frame, text="Result:")
-        self.result_label.grid(column=1, row=4, sticky=tk.W)
+        self.convert_button.grid(column=1, row=4, columnspan=2, pady=25) 
+
+        self.result_label = ttk.Label(main_frame, text="Result:", font='Arial 10')
+        self.result_label.grid(column=1, row=5, sticky=tk.W)
         
-        self.output_display = ttk.Label(main_frame, text="---", font='Arial 12 bold')
-        self.output_display.grid(column=2, row=4, sticky=(tk.W, tk.E))
+        self.output_display = ttk.Label(main_frame, text="---", font='Arial 14 bold', foreground="blue")
+        self.output_display.grid(column=2, row=5, sticky=(tk.W, tk.E))
 
     def run_conversion(self):
-        selected_name = self.dropdown_variable.get() 
-        code = conversion_codes[selected_name] 
-        value = self.input_entry.get() 
+        basic_choice = self.basic_var.get()
+        complex_choice = self.complex_var.get()
+        
+        if basic_choice:
+            code = basic_codes[basic_choice]
+        elif complex_choice:
+            code = complex_codes[complex_choice]
+        else:
+            self.output_display.config(text="Select a unit!", foreground="red")
+            return
+
+        value = self.input_entry.get()
+        try:
+            float(value)
+        except ValueError:
+            self.output_display.config(text="Invalid Number", foreground="red")
+            return
+
         final_result = perform_conversion_via_files(value, code)
-        self.output_display.config(text=final_result)
+        self.output_display.config(text=final_result, foreground="black")
 
 if __name__ == "__main__":
     root = tk.Tk()
--- unitconverter.c
+++ unitconverter.c
@@ -18,6 +18,30 @@ float c_to_k(float c){return c+273.15;}
 float k_to_c(float k){return k-273.15;}
 float l_to_ml(float l){return l*1000;}
 float ml_to_l(float ml){return ml/1000;}
+float j_to_cal(float j){return j/4.184;}
+float cal_to_j(float cal){return cal*4.184;}
+float pa_to_bar(float pa){return pa/100000;}
+float bar_to_pa(float bar){return bar*100000;}
+float pa_to_psi(float pa){return pa/6895;}
+float psi_to_pa(float psi){return psi*6895;}
+float torr_to_pa(float torr){return torr*133.322;}
+float pa_to_torr(float pa){return pa/133.322;}
+float ms_to_kmh(float ms){return ms*3.6;}
+float kmh_to_ms(float kmh){return kmh/3.6;}
+float mileps_to_kmh(float mileps){return mileps*5794;}
+float kmh_to_mileps(float kmh){return kmh/5794;}
+float gcm3_to_kgm3(float gcm3){return gcm3*1000;}
+float kgm3_to_gcm3(float kgm3){return kgm3/1000;}
+float hp_to_w(float hp){return hp*745.7;}
+float w_to_hp(float w){return w/745.7;}
+float Mbyteps_to_mbps(float Mbyteps){return Mbyteps/8;}
+float mbps_to_Mbyteps(float mbps){return mbps*8;}
+float mgdl_to_mmolL(float mgdl){return mgdl/18.018;}
+float mmolL_to_mgdl(float mmolL){return mmolL*18.018;}
+float gold_purity(float karat){return (karat/24)*100;}
+float knot_to_kmph(float knot){return knot*1.85;}
+float kmph_to_knot(float kmph){return kmph/1.85;}
+
 
 int main()
 {
@@ -111,6 +135,98 @@ int main()
 
         case 18:
         result=ml_to_l(input_value);
+        break;
+
+        case 19:
+        result=j_to_cal(input_value);
+        break;
+
+        case 20:
+        result=cal_to_j(input_value);
+        break;
+
+        case 21:
+        result=pa_to_bar(input_value);
+        break;
+
+        case 22:
+        result=bar_to_pa(input_value);
+        break;
+
+        case 23:
+        result=pa_to_psi(input_value);
+        break;
+
+        case 24:
+        result=psi_to_pa(input_value);
+        break;
+
+        case 25:
+        result=torr_to_pa(input_value);
+        break;
+
+        case 26:
+        result=pa_to_torr(input_value);
+        break;
+
+        case 27:
+        result=ms_to_kmh(input_value);
+        break;
+
+        case 28:
+        result=kmh_to_ms(input_value);
+        break;
+
+        case 29:
+        result=mileps_to_kmh(input_value);
+        break;
+
+        case 30:
+        result=kmh_to_mileps(input_value);
+        break;
+
+        case 31:
+        result=gcm3_to_kgm3(input_value);
+        break;
+
+        case 32:
+        result=kgm3_to_gcm3(input_value);
+        break;
+
+        case 33:
+        result=hp_to_w(input_value);
+        break;
+
+        case 34:
+        result=w_to_hp(input_value);
+        break;
+
+        case 35:
+        result=Mbyteps_to_mbps(input_value);
+        break;
+
+        case 36:
+        result=mbps_to_Mbyteps(input_value);
+        break;
+
+        case 37:
+        result=mgdl_to_mmolL(input_value);
+        break;
+
+        case 38:
+        result=mmolL_to_mgdl(input_value);
+        break;
+
+        case 39:
+        result=gold_purity(input_value);
+        break;
+
+        case 40:
+        result=knot_to_kmph(input_value);
+        break;
+
+        case 41:
+        result=kmph_to_knot(input_value);
         break;
 
         default:
